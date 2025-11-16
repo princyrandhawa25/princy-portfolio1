57f63d2d364611b0257e0c315efde6d99a0caaf5
princyrandhawa25 <princyrandhawa25@gmail.com>
2025-11-16 16:37:15 +0530

    Fix Tailwind v3 config for CRA

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
@@ -9,8 +12,8 @@ module.exports = {
         background: "#fffaf7",
       },
       fontFamily: {
-        heading: ["'Roboto'", "sans-serif"],
-        body: ["'Roboto'", "sans-serif"],
+        heading: ["Roboto", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
       },
     },
   },
