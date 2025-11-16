86ed54c8f61b52a0cb908adc89bae84104e467ac
princyrandhawa25 <princyrandhawa25@gmail.com>
2025-11-16 17:44:48 +0530

    Update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full py-6 mt-16 border-t border-accent bg-primary text-white">
+    <footer className="w-full py-6 mt-16 border-t border-accent bg-[#026559] text-white">
       <div className="max-w-5xl mx-auto px-4 text-sm/relaxed text-center">
         (c) {new Date().getFullYear()} Princy - All rights reserved.
       </div>
