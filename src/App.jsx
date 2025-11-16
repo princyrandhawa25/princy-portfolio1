563e93c1a3688511064e80510648c7724dd91260
princyrandhawa25 <princyrandhawa25@gmail.com>
2025-11-16 17:27:08 +0530

    Save local changes before pulling

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import InvitedTalksPage from "./pages/InvitedTalksPage";
 import PeerReviewPage from "./pages/PeerReviewPage";
 import PatentsPage from "./pages/PatentsPage";
 import Footer from "./components/Footer";
-import Loader from "./components/Loader";
+
 import { fetchPortfolioData, defaultData } from "./data/githubData";
 
 
